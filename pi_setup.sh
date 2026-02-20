cd5448d913775c64cd64484c66afa81cecdb28c5
Joe Ryan <joe@clanryan.us>
2026-02-20 14:24:08 -0500

    updated to skip network setup and updates

--- pi_setup.sh
+++ pi_setup.sh
@@ -25,16 +25,16 @@
 
 echo "Enter the node number of this pi followed by [ENTER]: "
 read pi_number
-re='^[0-9]+$'
+re='^[1-8]+$'
 if ! [[ $pi_number =~ $re ]] ; then
     echo "Error: Please enter integer" >&2; exit 1
 fi
 pi_name="pi$pi_number"
 
 echo "Installing system software and updates"
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install vim mpich2 xboxdrv libglew-dev sshpass libav-tools
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
+#sudo apt-get -y install vim mpich2 xboxdrv libglew-dev sshpass libav-tools
 
 echo "Setting computer name"
 for file in \
@@ -43,32 +43,35 @@ for file in \
   /etc/ssh/ssh_host_rsa_key.pub \
   /etc/ssh/ssh_host_dsa_key.pub
 do
-  [ -f $file ] && sudo sed -i -E "s/pi[0-9]+/$pi_name/" $file > /dev/null 2>&1
+  [ -f $file ] && sudo sed -i -E "s/pi[1-8]+/$pi_name/" $file > /dev/null 2>&1
   [ -f $file ] && sudo sed -i "s/raspberrypi/$pi_name/" $file > /dev/null 2>&1
 done
 sudo /etc/init.d/hostname.sh start > /dev/null 2>&1
 sudo hostname $pi_name
 
-echo "Setting network interface" 
-sudo tee /etc/network/interfaces <<-EOF
-auto lo
+echo "NOT Setting network interface" 
+#sudo tee /etc/network/interfaces <<-EOF
+#auto lo
  
-iface lo inet loopback
+#iface lo inet loopback
 # iface eth0 inet dhcp
  
-auto eth0
-iface eth0 inet static
-address 192.168.3.$(($pi_number+100))
-gateway 192.168.3.1
-netmask 255.255.255.0
-network 192.168.3.0
-broadcast 192.168.3.255
+#auto eth0
+#iface eth0 inet static
+#address 192.168.3.$(($pi_number+100))
+#gateway 192.168.3.1
+#netmask 255.255.255.0
+#network 192.168.3.0
+#broadcast 192.168.3.255
  
-# allow-hotplug wlan0
-# iface wlan0 inet manual
-# wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
-# iface default inet dhcp
-EOF
+#allow-hotplug wlan0
+#iface wlan0 inet manual
+#    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+#
+#allow-hotplug wlan1
+#iface wlan1 inet manual
+#    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+
  
 echo "Generating key-pairs"
 ssh-keygen -N '' -f /home/pi/.ssh/id_rsa
